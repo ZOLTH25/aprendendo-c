102f0d22d0ef7c140bce072352bcf7696285d42c
ZOLTH <cazeyage.bs250107@hotmail.com>
2025-11-07 00:26:09 -0300

    resolucao dos ex7, ex8 e inicio do ex9: Calcular Imc e notas e resto de um saque

--- lista1/ex7.c
+++ lista1/ex7.c
@@ -1,1 +1,34 @@
 //Crie um programa que solicite o peso (em kg) e a altura (em metros) de uma pessoa. Calcule o IMC usando a fórmula: IMC=peso/(altura∗altura). Com base no resultado, exiba a categoria correspondente:
+#include <stdio.h>
+#include <stdbool.h>
+
+float receberPesoAltura() {
+    float a;
+    scanf(" %f", &a);
+    fflush(stdin);
+    return a;
+}
+
+int main() {
+    float a;
+    float b;
+    float imc;
+    printf("Digite seu peso em kg :");
+    a = receberPesoAltura();
+    printf("Digite sua altura em metros :");
+    b = receberPesoAltura();
+    
+    imc=a/(b*b);
+    printf("sei imc e igual a : %.2f \n", imc);
+
+    if(imc < 18.5){
+        printf("Abaixo do peso");
+    }else if (imc >= 18.5 && imc <= 24.9){
+        printf("Peso normal");
+    }else if (imc >= 25.0 && imc <= 29.9){
+        printf("Sobrepeso");
+    }else {
+        printf("Obesidade");
+    }
+    return 0;
+}
---
+++ lista1/ex8.c
@@ -0,0 +1,67 @@
+//Simule um caixa eletrônico. Peça ao usuário para inserir um valor de saque (inteiro). O programa deve informar quantas notas de 
+// R100,R50, R20,R10, R5,R2 e R$1 são necessárias para compor o valor do saque, priorizando sempre as notas de maior valor.
+//82. 1: 50   1:20 1:10 e 1:2
+//82/100 .  0     82         82/50      1  32             32/20     1   12      12/10   1   2    2/2   1   0    
+//     int resto50;
+//    int resto20;
+//    int resto10;
+ //   int resto5;
+ //   int resto2;
+ //   int resto1;
+#include <stdio.h>
+#include <stdbool.h>
+
+int charmarNumero() {
+    int n;
+    printf("Insira o valor do saque: ");
+    scanf("%d", &n);
+    return n;
+}
+
+int main() {
+    int n1;
+    int div100 ;
+    int resto100;
+    int div50 ;
+    int resto50;  
+    int div20 ;
+    int resto20; 
+    int div10 ;
+    int resto10; 
+    int div5 ;
+    int resto5; 
+    int div2 ;
+    int resto2; 
+    int div1 ;
+    int resto1; 
+    n1 = charmarNumero();
+    div100= n1/100;
+    resto100= n1 % 100;
+    printf("As nota de 100 usadas sao : %d \n", div100);
+
+    div50= resto100/50;
+    resto50= resto100 % 50;
+    printf("As nota de 50 usadas sao : %d \n", div50);
+
+    div20= resto50/20;
+    resto20= resto50 % 20;
+    printf("As nota de 20 usadas sao : %d \n", div20);
+
+    div10= resto20/10;
+    resto10= resto20 % 10;
+    printf("As nota de 10 usadas sao : %d \n", div10);
+
+    div5= resto10/5;
+    resto5= resto10 % 5;
+    printf("As nota de 5 usadas sao : %d \n", div5);
+
+    div2= resto5/2;
+    resto5= resto5 % 2;
+    printf("As nota de 2 usadas sao : %d \n", div2);
+
+    div1= resto5/1;
+    resto2= resto5 % 1;
+    printf("As nota de 1 usadas sao : %d \n", div1);
+
+    return 0;
+}
